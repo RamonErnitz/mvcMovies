2c52ca03deb6dc12af8b209ac7ce6ed386ac0f0c
Opilane TTHK <opilane@tthk.ee>
2021-10-14 11:17:01 +0300

    MvC

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace mvcAlexKren.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string Hacker()
+        {
+            return "You got hacked bae :)";
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1, int Id= 3 * 2)
+        {
+            ViewData["Message"] = $"Hello {name} Sinu id on {Id}";
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+        
+            
+    }
+}
